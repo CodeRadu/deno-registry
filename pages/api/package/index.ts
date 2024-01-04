8ea4153f75181ef25aa9f415593c1117be4c13f2
Radu <47945947+CodeRadu@users.noreply.github.com>
2024-01-04 16:22:22 +0000

    whoops

--- pages/api/package/index.ts
+++ pages/api/package/index.ts
@@ -1,10 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getAllPackages } from "../../../database";
+import { getAllPackages, addPackage } from "../../../database";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const method = req.method
-  if (method === "GET") {
-    const packages = await getAllPackages()
-    res.status(200).json(packages)
-  }
+  if (method === "POST") {
+    const body = JSON.parse(req.body)
+    const pkg = addPackage({
+      name: body.name,
+    })
+    res.status(200).json(pkg)
+  } else
+    if (method === "GET") {
+      const packages = await getAllPackages()
+      res.status(200).json(packages)
+    }
 }
