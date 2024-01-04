84d6b4b93ec6284c3efc34b2795d6f2273b8ce34
Radu <47945947+CodeRadu@users.noreply.github.com>
2024-01-04 16:32:51 +0000

    change PackageList component

--- components/PackageList.tsx
+++ components/PackageList.tsx
@@ -1,45 +1,48 @@
-import { Package } from '@prisma/client'
-import Link from 'next/link'
-import React, { useEffect } from 'react'
+import { Package } from "@prisma/client";
+import Link from "next/link";
+import React, { useEffect } from "react";
 
 interface Props {
-  pkgs?: Package[]
+  pkgs?: Package[];
 }
 
 export default function PackageList({ pkgs }: Props) {
   useEffect(() => {
-    console.log(pkgs)
-  }, [pkgs])
+    console.log(pkgs);
+  }, [pkgs]);
   return (
     <div>
       {pkgs?.length == 0 || !pkgs ? (
-        'No packages found'
+        "No packages found"
       ) : (
-        <table className=''>
+        <table className="">
           <thead>
             <tr>
-              <th className='td'>Package Name</th>
-              <th className='td'>Repository</th>
-              <th className='td'>Use path</th>
+              <th className="td">Package Name</th>
+              <th className="td">Repository</th>
+              <th className="td">Use path</th>
             </tr>
           </thead>
           <tbody>
             {pkgs.map((pkg) => {
               return (
                 <tr key={pkg.id}>
-                  <td className='td'>{pkg.name}</td>
-                  <td className='td'>
+                  <td className="td">{pkg.name}</td>
+                  <td className="td">
                     <Link href={pkg.repository}>
-                      {pkg.repository || 'unknown'}
+                      {pkg.repository || "unknown"}
                     </Link>
                   </td>
-                  <td className='td'>{`https://reg.venovedo.ro/api/${pkg.name}/<file>`}</td>
+                  <td className="td">
+                    {typeof window !== "undefined" &&
+                      `${window.location.origin}/api/${pkg.name}/<file>`}
+                  </td>
                 </tr>
-              )
+              );
             })}
           </tbody>
         </table>
       )}
     </div>
-  )
+  );
 }
